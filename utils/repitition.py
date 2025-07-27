687e20844d7b906899c27c8bcb831cebdf508283
HeckerWithAHat <aryanj1010@gmail.com>
2025-07-27 16:57:06 -0700

    Co-authored-by: Shizy10 <Shizy10@users.noreply.github.com> Co-authored-by: Mihir Bapat <Hypernova101@users.noreply.github.com>

--- utils/repitition.py
+++ utils/repitition.py
@@ -1,20 +1,3 @@
-def repetition_encode(bit, n):
-    """
-    Encode bits using repetition code of length n.
-    Example: bit=1, n=3 => [1,1,1]
-    """
-    return [bit] * n
+def repetition_encode(bit, n): return [bit] * n
 
-
-def repetition_decode(codeword, n):
-    """
-    Decode bits by majority voting.
-    """
-    # Count the number of 1s in the codeword
-    ones_count = sum(codeword)
-    
-    # If more than half are 1s, decode to 1; otherwise decode to 0
-    if ones_count > n // 2:
-        return 1
-    else:
-        return 0
+def repetition_decode(codeword, n): return 1 if sum(codeword) > n // 2 else 0
