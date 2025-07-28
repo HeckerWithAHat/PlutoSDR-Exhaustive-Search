d9f4af64686d40725dbf93876a4974effbb1d3a0
Hypernova101 <mihirnbapat@gmail.com>
2025-07-28 13:13:50 -0700

    update with new scheme

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from utils.hamming import create_hamming_code_matrices
-from utils.hamming import hamming_decode
+from utils.recovery.hamming import create_hamming_code_matrices
+from utils.recovery.hamming import hamming_decode
 
-from utils.huffman import huffman_encoding
-from utils.huffman import huffman_decoding
+from utils.compression.huffman import huffman_encoding
+from utils.compression.huffman import huffman_decoding
 
-from utils.lempelziv import lempel_ziv_encoding
-from utils.lempelziv import lempel_ziv_decoding
+from utils.compression.lempelziv import lempel_ziv_encoding
+from utils.compression.lempelziv import lempel_ziv_decoding
 
-from utils.reedsolomon import create_reed_solomon_generator_matrix
-from utils.reedsolomon import decode_rs_erasure
+from utils.recovery.reedsolomon import create_reed_solomon_generator_matrix
+from utils.recovery.reedsolomon import decode_rs_erasure
 
-from utils.repitition import repetition_encode
-from utils.repitition import repetition_decode
+from utils.recovery.repitition import repetition_encode
+from utils.recovery.repitition import repetition_decode
 
 
