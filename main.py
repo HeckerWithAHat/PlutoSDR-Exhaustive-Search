9ae038beb09080b4332c992800cc83d847639e7d
HeckerWithAHat <aryanj1010@gmail.com>
2025-07-30 09:34:15 -0700

    Fix Lempel Ziv. All Compression and Recovery Working

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from utils.enums.enums import Compression, Recovery, Constellation, SymbolErrorP
 
 sys.set_int_max_str_digits(0) # Disable the limit on the number of digits in an integer
 
-simulate_local(Compression.HUFFMAN, Recovery.HAMMING, Constellation.QAM8, Repitition.R1, "./files/text.txt")
+simulate_local(Compression.LEMPEL_ZIV, Recovery.REPETITION, Constellation.QAM8, Repitition.R1, "./files/text.txt")
